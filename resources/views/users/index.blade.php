3739864449744e683c15846b52d1b7147c9ade79
Gabriel Mariano <gabrielmbdev@gmail.com>
2022-07-11 22:16:51 -0300

    criacao de rotas index e show | criacao de view index e show | criacao de controller user

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listagem de Usuários</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1>Listagem de Usuários</h1>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID#</th>
+      <th scope="col">Nome</th>
+      <th scope="col">Email</th>
+      <th scope="col">Data de Cadastro</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($users as $user)
+    <tr>
+      <th scope="row">{{ $user->id}}</th>
+      <td>{{ $user->name}}</td>
+      <td>{{$user->email}}</td>
+      <td>{{$user->created_at}}</td>
+    </tr>
+    @endforeach
+
+  </tbody>
+</table>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
