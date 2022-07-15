97f94dbafc353813179a06c2bf51b79768cf0088
Gabriel Mariano <gabrielmbdev@gmail.com>
2022-07-14 21:13:31 -0300

    adicionado template users

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listagem de Usuários</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
+@extends('template.users')
+@section('title', 'Visualizar Usuário')
+@section('body')
         <h1>Listagem de Usuários</h1>
         <table class="table">
   <thead>
@@ -24,11 +16,9 @@
       <th scope="row">{{ $user->id}}</th>
       <td>{{ $user->name}}</td>
       <td>{{$user->email}}</td>
-      <td>{{$user->created_at}}</td>
+      <td>{{date('d/m/Y - H:i', strtotime($user->created_at))}}</td>
     </tr>
 
   </tbody>
 </table>
-    </div>
-</body>
-</html>
+@endsection
