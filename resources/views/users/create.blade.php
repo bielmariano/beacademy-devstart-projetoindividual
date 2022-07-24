e16c9d1919d40f905217ea02e99c871b06942ad6
Gabriel Mariano <gabrielmbdev@gmail.com>
2022-07-24 12:34:34 -0300

    adicionado outros atributos para o form create user e show user

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -14,6 +14,23 @@
     <input type="email" class="form-control" id="email" name="email">
   </div>
   <div class="mb-3">
+    <label for="date" class="form-label">Data de Nascimento</label>
+    <input type="date" class="form-control" id="date" name="date">
+  </div>
+  <div class="mb-3">
+    <label for="address" class="form-label">Endereço</label>
+    <input type="text" class="form-control" id="address" name="address">
+  </div>
+  <div class="mb-3">
+    <label for="phone" class="form-label">Telefone</label>
+    <input type="tel" class="form-control" id="phone" name="phone">
+  </div>
+
+  <div class="mb-3">
+    <label for="occupation" class="form-label">Ocupação</label>
+    <input type="text" class="form-control" id="occupation" name="occupation">
+  </div>
+  <div class="mb-3">
     <label for="cpf" class="form-label">CPF</label>
     <input type="text" class="form-control" id="cpf" name="cpf">
   </div>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,6 +8,11 @@
       <th scope="col">ID#</th>
       <th scope="col">Nome</th>
       <th scope="col">Email</th>
+      <th scope="col">Aniversário</th>
+      <th scope="col">Endereço</th>
+      <th scope="col">Telefone</th>
+      <th scope="col">Ocupação</th>
+      <th scope="col">CPF</th>
       <th scope="col">Data de Cadastro</th>
     </tr>
   </thead>
@@ -16,6 +21,11 @@
       <th scope="row">{{ $user->id}}</th>
       <td>{{ $user->name}}</td>
       <td>{{$user->email}}</td>
+      <td>{{$user->birthday}}</td>
+      <td>{{$user->address}}</td>
+      <td>{{$user->phone}}</td>
+      <td>{{$user->occupation}}</td>
+      <td>{{$user->cpf}}</td>
       <td>{{date('d/m/Y - H:i', strtotime($user->created_at))}}</td>
     </tr>
 
