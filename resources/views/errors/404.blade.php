c30d091e6e8f19080c835075bde020a38b0f6b0f
Gabriel Mariano <gabrielmbdev@gmail.com>
2022-08-02 15:48:29 -0300

    imagem da página 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,9 @@
-<h1>Exception do usuário</h1>
+@extends('template.users')
+@section('title', 'Dashboard')
+@section ('body')
+
+<div class="card mx-auto" style="width: 50rem;">
+  <img src="{{ asset('storage/404image.png') }}" class="" alt="Página não encontrada">
+</div>
+
+@endsection
