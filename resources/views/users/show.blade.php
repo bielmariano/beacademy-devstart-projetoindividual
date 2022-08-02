bed114eebf7c16281daa9b11f656284bf4465960
Gabriel Mariano <gabrielmbdev@gmail.com>
2022-08-02 18:04:55 -0300

    melhorias na view show

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,50 +1,69 @@
 @extends('template.users')
 @section('title', 'Visualizar Usuário')
 @section('body')
-<h1>Informações de usuário</h1>
+<h1>Informações do usuário</h1>
 
-<table class="table">
-  <thead class="text-center">
-    <tr>
-      <th scope="col">Image</th>
-      <th scope="col">ID#</th>
-      <th scope="col">Nome</th>
-      <th scope="col">Email</th>
-      <th scope="col">Aniversário</th>
-      <th scope="col">Endereço</th>
-      <th scope="col">Telefone</th>
-      <th scope="col">Ocupação</th>
-      <th scope="col">CPF</th>
-      <th scope="col">Data de Cadastro</th>
-      <th scope="col">Mais</th>
-    </tr>
-  </thead>
-  <tbody class="text-center">
-    <tr>
-      @if($user->image)
-      <th> <img src="{{asset('storage/'.$user->image)}}" alt="" width="50px" height="50px"> </th>
-      @else
-      <th> <img src="{{asset('storage/profile/unisexprofile.jpg')}}" alt="" width="50px" height="50px"> </th>
-      @endif
-      <th scope="row">{{ $user->id}}</th>
-      <td>{{ $user->name}}</td>
-      <td>{{$user->email}}</td>
-      <td>{{$user->birthday}}</td>
-      <td>{{$user->address}}</td>
-      <td>{{$user->phone}}</td>
-      <td>{{$user->occupation}}</td>
-      <td>{{$user->cpf}}</td>
-      <td>{{date('d/m/Y - H:i', strtotime($user->created_at))}}</td>
-      <td> <a href="{{route('users.edit', $user->id)}}" class="btn btn-warning text-white">Editar</a></td>
-      <td>
-        <form action="{{route('users.destroy', $user->id)}}" method="POST">
-          @method('DELETE')
-          @csrf
-          <button class="btn btn-danger text-white">Deletar</button>
-        </form>
-      </td>
-    </tr>
+<section class="section gray-bg" id="about">
+  <div class="container">
+    <div class="row align-items-center flex-row-reverse">
+      <div class="col-lg-6">
+        <div class="about-text go-to">
+          <h3 class="dark-color">{{$user->name}}</h3>
+          <a href="{{ route('users.edit', $user->id) }}" class="btn btn-warning text-white mb-2">EDITAR</a>
+          <form action="{{ route('users.destroy', $user->id) }}" method="POST">
+            @method('DELETE')
+            @csrf
+            <button class="btn btn-danger text-white">DELETAR</button>
+          </form>
+          <div class="row about-list">
+            <div class="col-md-6">
+              <div class="media">
+                <label>#ID</label>
+                <p>{{ $user->id }}</p>
+              </div>
+              <div class="media">
+                <label>Aniversário</label>
+                <p>{{date('d/m/Y', strtotime($user->birthday))}}</p>
+              </div>
+              <div class="media">
+                <label>E-mail</label>
+                <p>{{ $user->email }}</p>
+              </div>
+              <div class="media">
+                <label>Endereço</label>
+                <p>{{ $user->address }}</p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="media">
+                <label>Telefone</label>
+                <p>{{ $user->phone }}</p>
+              </div>
+              <div class="media">
+                <label>Ocupação</label>
+                <p>{{ $user->occupation }}</p>
+              </div>
+              <div class="media">
+                <label>CPF</label>
+                <p>{{ $user->cpf }}</p>
+              </div>
+              <div class="media">
+                <label>Data de cadastro</label>
+                <p>{{date('d/m/Y - H:i', strtotime($user->created_at))}}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        @if($user->image)
+        <div> <img src="{{asset('storage/'.$user->image)}}" alt="" width="250px" height="250"> </div>
+        @else
+        <div> <img src="{{asset('storage/profile/unisexprofile.jpg')}}" alt="" width="250" height="250"> </div>
+        @endif
+      </div>
+    </div>
 
-  </tbody>
-</table>
+  </div>
+</section>
 @endsection
